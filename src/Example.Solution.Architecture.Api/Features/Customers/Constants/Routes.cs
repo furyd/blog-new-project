2ca0588234736ecef70de0cd514fac9b8f623c8d
Dave Fury <david.fury@reservoir-devs.com>
2024-10-07 14:35:56 +0100

    feat: updated to add all planned endpoints

--- src/Example.Solution.Architecture.Api/Features/Customers/Constants/Routes.cs
+++ src/Example.Solution.Architecture.Api/Features/Customers/Constants/Routes.cs
@@ -3,4 +3,14 @@
 public static class Routes
 {
     public const string Root = "customers";
+
+    public const string List = Root;
+
+    public const string Get = Root + "/{id}";
+
+    public const string Create = Root;
+
+    public const string Replace = Root + "/{id}";
+
+    public const string Delete = Root + "/{id}";
 }
