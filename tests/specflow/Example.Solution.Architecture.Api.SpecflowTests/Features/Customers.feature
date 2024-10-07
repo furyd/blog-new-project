7a715419c5a87df736ca94fe4fd40bac7591ef1e
Dave Fury <david.fury@reservoir-devs.com>
2024-10-07 14:38:25 +0100

    tests: added specflow tests for new endpoint

--- tests/specflow/Example.Solution.Architecture.Api.SpecflowTests/Features/Customers.feature
+++ tests/specflow/Example.Solution.Architecture.Api.SpecflowTests/Features/Customers.feature
@@ -1,9 +1,32 @@
 ﻿Feature: Customers
 
-Returns a OK status
+Actions on the Customers entity
 
-Scenario: Make a Customers request
-	Given I am calling the Customers endpoint
+Scenario: List all cutomers
+	Given I am calling the Customers list endpoint
 	When I make a GET request
-	Then I should get a 200 response
-	And a list of customers
+	Then I should get either a 200 or 204 response
+
+Scenario: Get a customer by ID
+	Given I am calling the Customers get endpoint with the ID 00000000-0000-0000-0000-000000000000
+	When I make a GET request
+	Then I should get a 404 response
+
+Scenario: Create a new customer
+	Given I am calling the Customers create endpoint
+	When I make a POST request with the following data
+		| GivenName | FamilyName |
+		| A | B |
+	Then I should get a 201 response
+
+Scenario: Update a customer
+	Given I am calling the Customers update endpoint with the ID 00000000-0000-0000-0000-000000000000
+	When I make a PUT request with the following data
+		| GivenName | FamilyName |
+		| A | B |
+	Then I should get a 404 response
+
+Scenario: Delete a customer
+	Given I am calling the Customers delete endpoint with the ID 00000000-0000-0000-0000-000000000000
+	When I make a DELETE request
+	Then I should get a 404 response
--- tests/specflow/Example.Solution.Architecture.Api.SpecflowTests/StepDefinitions/CustomersStepDefinitions.cs
+++ tests/specflow/Example.Solution.Architecture.Api.SpecflowTests/StepDefinitions/CustomersStepDefinitions.cs
@@ -1,29 +1,98 @@
-using NUnit.Framework;
-using System.Net;
-using System.Text.Json;
 using Example.Solution.Architecture.Api.Features.Customers.Constants;
 using Example.Solution.Architecture.Api.Features.Customers.Models.Responses;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace Example.Solution.Architecture.Api.SpecflowTests.StepDefinitions;
 
 [Binding]
 public sealed class CustomersStepDefinitions : IDisposable
 {
-    private string _url = TestContext.Parameters.Get<string>("webAppUrl", string.Empty);
+    private readonly string _baseUrl = TestContext.Parameters.Get<string>("webAppUrl", string.Empty);
+    private string _endpoint = "";
     private HttpResponseMessage? _response;
 
-    [Given("I am calling the Customers endpoint")]
+    [Given("I am calling the Customers list endpoint")]
     public void GivenIAmCallingTheCustomersEndpoint()
     {
-        var url = TestContext.Parameters.Get<string>("webAppUrl", string.Empty);
-        _url = $"{url}/{Routes.Root}";
+        _endpoint = Routes.List;
     }
+
+    [Given("I am calling the Customers get endpoint with the ID (.*)")]
+    public void GivenIAmCallingTheCustomersGetEndpoint(string id)
+    {
+        _endpoint = Routes.Get.Replace("{id}", id);
+    }
+
+    [Given("I am calling the Customers create endpoint")]
+    public void GivenIAmCallingTheCustomersCreateEndpoint()
+    {
+        _endpoint = Routes.Create;
+    }
+
+    [Given("I am calling the Customers update endpoint with the ID (.*)")]
+    public void GivenIAmCallingTheCustomersUpdateEndpoint(string id)
+    {
+        _endpoint = Routes.Replace.Replace("{id}", id);
+    }
+
+    [Given("I am calling the Customers delete endpoint with the ID (.*)")]
+    public void GivenIAmCallingTheCustomersDeleteEndpoint(string id)
+    {
+        _endpoint = Routes.Delete.Replace("{id}", id);
+    }
+
 
     [When("I make a GET request")]
     public async Task WhenIMakeAGetRequest()
     {
-        using var client = new HttpClient();
-        _response = await client.GetAsync(_url);
+        using var message = new HttpRequestMessage(HttpMethod.Get, _endpoint);
+        await WhenIMakeARequest(message);
+    }
+
+    [When("I make a POST request with the following data")]
+    public async Task WhenIMakeAPostRequestWithTheFollowingData(Table table)
+    {
+        using var message = new HttpRequestMessage(HttpMethod.Post, _endpoint);
+
+        foreach (var tableRow in table.Rows)
+        {
+            var model = new Customer
+            {
+                GivenName = tableRow["GivenName"],
+                FamilyName = tableRow["FamilyName"]
+            };
+
+            message.Content = JsonContent.Create(model);
+            await WhenIMakeARequest(message);
+        }
+    }
+
+    [When("I make a PUT request with the following data")]
+    public async Task WhenIMakeAPutRequest(Table table)
+    {
+        using var message = new HttpRequestMessage(HttpMethod.Put, _endpoint);
+
+        foreach (var tableRow in table.Rows)
+        {
+            var model = new Customer
+            {
+                GivenName = tableRow["GivenName"],
+                FamilyName = tableRow["FamilyName"]
+            };
+
+            message.Content = JsonContent.Create(model);
+            await WhenIMakeARequest(message);
+        }
+    }
+
+    [When("I make a DELETE request")]
+    public async Task WhenIMakeADeleteRequest()
+    {
+        using var message = new HttpRequestMessage(HttpMethod.Delete, _endpoint);
+        await WhenIMakeARequest(message);
     }
 
     [Then("I should get a (.*) response")]
@@ -33,7 +102,14 @@ public sealed class CustomersStepDefinitions : IDisposable
         _response?.StatusCode.Should().Be((HttpStatusCode)statusCode);
     }
 
-    [Then(@"a list of customers")]
+    [Then("I should get either a (.*) or (.*) response")]
+    public void ThenIShouldGetAResponse(int statusCode, int alternativeStatusCode)
+    {
+        _response.Should().NotBeNull();
+        _response?.StatusCode.Should().BeOneOf((HttpStatusCode)statusCode, (HttpStatusCode)alternativeStatusCode);
+    }
+
+    [Then("a list of customers")]
     public async Task ThenAListOfCustomers()
     {
         var content = await JsonSerializer.DeserializeAsync<List<Customer>>(await _response!.Content.ReadAsStreamAsync());
@@ -41,6 +117,12 @@ public sealed class CustomersStepDefinitions : IDisposable
         content.Should().NotBeNullOrEmpty();
     }
 
+    private async Task WhenIMakeARequest(HttpRequestMessage message)
+    {
+        using var client = new HttpClient();
+        client.BaseAddress = new Uri(_baseUrl);
+        _response = await client.SendAsync(message);
+    }
 
     public void Dispose()
     {
