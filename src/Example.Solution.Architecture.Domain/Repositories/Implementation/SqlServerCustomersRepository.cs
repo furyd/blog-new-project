b70afb4350562ce05cbc7832f15187b84d2f8ccf
Dave Fury <david.fury@reservoir-devs.com>
2024-10-09 10:38:05 +0100

    feat: added repository classes for Customers

---
+++ src/Example.Solution.Architecture.Domain/Repositories/Implementation/SqlServerCustomersRepository.cs
@@ -0,0 +1,92 @@
+﻿using Dapper;
+using Example.Solution.Architecture.Domain.Factories.Interfaces;
+using Example.Solution.Architecture.Domain.Repositories.Interfaces;
+using Example.Solution.Architecture.Domain.Repositories.Models;
+
+namespace Example.Solution.Architecture.Domain.Repositories.Implementation;
+
+public class SqlServerCustomersRepository(IConnectionFactory factory) : ICustomersRepository
+{
+    public async Task<IReadOnlyCollection<ICustomer>> List()
+    {
+        const string sql = """
+                           SELECT
+                                [Id]
+                           ,    [GivenName]
+                           ,    [FamilyName]
+                           FROM [Customers]
+                           """;
+
+        await using var connection = await factory.Create();
+
+        return (await connection.QueryAsync<Customer>(sql)).ToList().AsReadOnly();
+    }
+
+    public async Task<ICustomer?> Get(Guid id)
+    {
+        const string sql = """
+                           SELECT
+                                [Id]
+                           ,    [GivenName]
+                           ,    [FamilyName]
+                           FROM [Customers]
+                           WHERE 1=1
+                           AND [RowId] = @Id
+                           """;
+
+        await using var connection = await factory.Create();
+
+        return await connection.QuerySingleOrDefaultAsync<Customer>(sql, new { id });
+    }
+
+    public async Task<Guid> Create(ICreateCustomer model)
+    {
+        const string sql = """
+                           INSERT INTO [Customers]
+                           (
+                                    [GivenName]
+                               ,    [FamilyName]
+                           )
+                           OUTPUT [INSERTED].[Id]
+                           VALUES
+                           (
+                                    @GivenName
+                               ,    @FamilyName
+                           )
+                           """;
+
+        await using var connection = await factory.Create();
+
+        return await connection.QuerySingleAsync<Guid>(sql, new { model.GivenName, model.FamilyName });
+    }
+
+    public async Task Update(Guid id, ICreateCustomer model)
+    {
+        const string sql = """
+                           UPDATE [Customers]
+                           SET
+                               [GivenName] = @GivenName,
+                               [FamilyName] = @FamilyName
+                           WHERE 1=1
+                           AND [Id] = @Id
+                           """;
+
+        await using var connection = await factory.Create();
+
+        await connection.ExecuteAsync(sql, new { id, model.GivenName, model.FamilyName });
+    }
+
+    public async Task Delete(Guid id)
+    {
+        const string sql = """
+                           DELETE
+                           FROM [Customers]
+                           WHERE 1=1
+                           AND [Id] = @Id
+                           """;
+
+        await using var connection = await factory.Create();
+
+        await connection.ExecuteAsync(sql, new { id });
+    }
+}
---
+++ src/Example.Solution.Architecture.Domain/Repositories/Interfaces/ICustomersRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Example.Solution.Architecture.Domain.Repositories.Interfaces;
+
+public interface ICustomersRepository
+{
+    Task<IReadOnlyCollection<ICustomer>> List();
+
+    Task<ICustomer?> Get(Guid id);
+
+    Task<Guid> Create(ICreateCustomer model);
+
+    Task Update(Guid id, ICreateCustomer model);
+
+    Task Delete(Guid id);
+}
