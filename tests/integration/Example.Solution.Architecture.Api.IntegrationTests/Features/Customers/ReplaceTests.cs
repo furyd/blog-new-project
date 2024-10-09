f4afd9f72d0e55940bca3d4404e8dec36bd9ef34
Dave Fury <david.fury@reservoir-devs.com>
2024-10-09 10:52:11 +0100

    refcator: updated to use custom WebApplicationFactory

--- tests/integration/Example.Solution.Architecture.Api.IntegrationTests/Features/Customers/ReplaceTests.cs
+++ tests/integration/Example.Solution.Architecture.Api.IntegrationTests/Features/Customers/ReplaceTests.cs
@@ -1,20 +1,22 @@
 using Example.Solution.Architecture.Api.Features.Customers.Constants;
+using Example.Solution.Architecture.Api.IntegrationTests.WebApplicationFactories;
 using FluentAssertions;
-using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
 using System.Net.Http.Json;
 
 namespace Example.Solution.Architecture.Api.IntegrationTests.Features.Customers;
 
-public class ReplaceTests(WebApplicationFactory<Program> webApplicationFactory) : IClassFixture<WebApplicationFactory<Program>>
+public class ReplaceTests
 {
     [Fact]
-    public async Task Replace_ReturnsOKOrNotFound()
+    public async Task Replace_ReturnsOK()
     {
+        await using var webApplicationFactory = IntegrationTestingWebApplicationFactory.Create();
+
         using var client = webApplicationFactory.CreateClient();
 
         var sut = await client.PutAsJsonAsync(Routes.Replace.Replace("{id}", Guid.NewGuid().ToString("D")), new Api.Features.Customers.Models.Requests.Customer { GivenName = "A", FamilyName = "B" });
 
-        sut.StatusCode.Should().BeOneOf(HttpStatusCode.OK, HttpStatusCode.NotFound);
+        sut.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
