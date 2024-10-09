616f5a1f31b9516285561a1ef8c48e7e8f7fd211
Dave Fury <david.fury@reservoir-devs.com>
2024-10-09 10:41:48 +0100

    feat: added route names so that actions can reference one another, e.g. create action referencing get action for Location header

---
+++ src/Example.Solution.Architecture.Api/Features/Customers/Constants/RouteNames.cs
@@ -0,0 +1,14 @@
+﻿namespace Example.Solution.Architecture.Api.Features.Customers.Constants;
+
+public static class RouteNames
+{
+    public const string ListCustomers = nameof(ListCustomers);
+
+    public const string GetCustomerById = nameof(GetCustomerById);
+
+    public const string CreateCustomer = nameof(CreateCustomer);
+
+    public const string ReplaceCustomer = nameof(ReplaceCustomer);
+
+    public const string DeleteCustomer = nameof(DeleteCustomer);
+}
--- src/Example.Solution.Architecture.Api/Features/Customers/Registration/ComposeDependencies.cs
+++ src/Example.Solution.Architecture.Api/Features/Customers/Registration/ComposeDependencies.cs
@@ -8,22 +8,27 @@ public static class ComposeDependencies
     {
         webApplication
             .MapGet(Routes.List, Controllers.Customers.List)
+            .WithName(RouteNames.ListCustomers)
             ;
 
         webApplication
             .MapGet(Routes.Get, Controllers.Customers.Get)
+            .WithName(RouteNames.GetCustomerById)
             ;
 
         webApplication
             .MapPost(Routes.Create, Controllers.Customers.Create)
+            .WithName(RouteNames.CreateCustomer)
             ;
 
         webApplication
             .MapPut(Routes.Replace, Controllers.Customers.Replace)
+            .WithName(RouteNames.ReplaceCustomer)
             ;
 
         webApplication
             .MapDelete(Routes.Delete, Controllers.Customers.Delete)
+            .WithName(RouteNames.DeleteCustomer)
             ;
     }
 }
