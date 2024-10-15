07d2059d535e29da6bc4360a3eb119f08d1f3d52
furyd <david.fury@reservoir-devs.com>
2024-10-15 15:01:23 +0100

    feat: created a factory for generating a HttpContext for the unit tests

---
+++ tests/unit/Example.Solution.Architecture.Api.UnitTests/Factories/HttpContextFactory.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Example.Solution.Architecture.Api.UnitTests.Factories;
+
+internal class MockLinkGenerator : LinkGenerator
+{
+    public override string GetPathByAddress<TAddress>(HttpContext httpContext, TAddress address, RouteValueDictionary values,
+        RouteValueDictionary? ambientValues = null, PathString? pathBase = null,
+        FragmentString fragment = new(), LinkOptions? options = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetPathByAddress<TAddress>(TAddress address, RouteValueDictionary values,
+        PathString pathBase = new(), FragmentString fragment = new(),
+        LinkOptions? options = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string GetUriByAddress<TAddress>(HttpContext httpContext, TAddress address, RouteValueDictionary values,
+        RouteValueDictionary? ambientValues = null, string? scheme = null, HostString? host = null,
+        PathString? pathBase = null, FragmentString fragment = new(), LinkOptions? options = null)
+    {
+        return "A";
+    }
+
+    public override string GetUriByAddress<TAddress>(TAddress address, RouteValueDictionary values, string scheme, HostString host,
+        PathString pathBase = new(), FragmentString fragment = new(),
+        LinkOptions? options = null)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public static class HttpContextFactory
+{
+    public static HttpContext Create()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        var featureCollection = new FeatureCollection();
+
+        serviceCollection.AddLogging();
+        serviceCollection.AddRouting();
+        serviceCollection.AddSingleton<LinkGenerator, MockLinkGenerator>();
+
+        featureCollection.Set<IHttpResponseFeature>(new HttpResponseFeature());
+        featureCollection.Set<IHttpResponseBodyFeature>(new StreamResponseBodyFeature(new MemoryStream()));
+
+        return new DefaultHttpContext(featureCollection)
+        {
+            RequestServices = serviceCollection.BuildServiceProvider()
+        };
+    }
+}
