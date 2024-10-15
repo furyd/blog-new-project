8e88d064949ffe7835793a2aafe283205d436719
furyd <david.fury@reservoir-devs.com>
2024-10-15 14:54:29 +0100

    feat: added new custom type for returning JSON direct from a query result

---
+++ src/Example.Solution.Architecture.Api/CustomResults/JsonResult.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace Example.Solution.Architecture.Api.CustomResults;
+
+public class JsonResult(IEnumerable<string> results) : IResult
+{
+    public async Task ExecuteAsync(HttpContext httpContext)
+    {
+        httpContext.Response.ContentType = System.Net.Mime.MediaTypeNames.Application.Json;
+
+        foreach (var result in results)
+        {
+            await httpContext.Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(result));
+        }
+    }
+}
