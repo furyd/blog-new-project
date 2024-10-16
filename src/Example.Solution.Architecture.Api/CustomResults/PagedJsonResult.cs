81382c7ab37eeebc21e759c2aa181eaad9f3a55e
furyd <david.fury@reservoir-devs.com>
2024-10-16 16:18:12 +0100

    feat: added custom result for paged JSON data

---
+++ src/Example.Solution.Architecture.Api/CustomResults/PagedJsonResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Example.Solution.Architecture.Api.CustomResults;
+
+public class PagedJsonResult(IEnumerable<string> results, IEnumerable<string> links) : IResult
+{
+    public async Task ExecuteAsync(HttpContext httpContext)
+    {
+        httpContext.Response.ContentType = System.Net.Mime.MediaTypeNames.Application.Json;
+        httpContext.Response.Headers.Link = new Microsoft.Extensions.Primitives.StringValues(links.ToArray());
+
+        foreach (var result in results)
+        {
+            await httpContext.Response.BodyWriter.WriteAsync(Encoding.UTF8.GetBytes(result));
+        }
+    }
+}
