2af6e3c703fc27c7d39dbccc6e88818396315030
furyd <david.fury@reservoir-devs.com>
2024-10-16 16:17:08 +0100

    feat: added constants for query string and link relations

---
+++ src/Example.Solution.Architecture.Api/Constants/LinkRelations.cs
@@ -0,0 +1,14 @@
+﻿namespace Example.Solution.Architecture.Api.Constants;
+
+public static class LinkRelations
+{
+    public const string First = "first";
+
+    public const string Last = "last";
+
+    public const string Previous = "prev";
+
+    public const string Next = "next";
+
+    public const string Item = "item";
+}
---
+++ src/Example.Solution.Architecture.Api/Constants/QueryStrings.cs
@@ -0,0 +1,8 @@
+﻿namespace Example.Solution.Architecture.Api.Constants;
+
+public static class QueryStrings
+{
+    public const string PageNumber = "page";
+
+    public const string PageSize = "page-size";
+}
