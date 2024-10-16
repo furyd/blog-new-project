fa85e635ef841230f82be5cb33250cdd4e35578e
furyd <david.fury@reservoir-devs.com>
2024-10-16 16:19:14 +0100

    feat: added model and interface for returning paged data

---
+++ src/Example.Solution.Architecture.Domain/Abstractions/IPagedResults.cs
@@ -0,0 +1,8 @@
+﻿namespace Example.Solution.Architecture.Domain.Abstractions;
+
+public interface IPagedResults<out T>
+{
+    IReadOnlyCollection<T> Data { get; }
+
+    int RecordCount { get; }
+}
---
+++ src/Example.Solution.Architecture.Domain/Repositories/Models/PagedData.cs
@@ -0,0 +1,5 @@
+﻿using Example.Solution.Architecture.Domain.Abstractions;
+
+namespace Example.Solution.Architecture.Domain.Repositories.Models;
+
+public record PagedData<T>(int RecordCount, IReadOnlyCollection<T> Data) : IPagedResults<T>;
